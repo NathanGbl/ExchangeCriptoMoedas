aadddd9adc6133d14941e9e9dc203c4aeed2e1fd
NathanGbl <nathangabriel.f.leite@gmail.com>
2024-05-09 15:06:36 -0300

    Criação das classes Moedas, Investidor, Carteira e Pessoa. Falta estabelecer melhor as relação entre as classes.

---
+++ src/Model/Moedas.java
@@ -0,0 +1,10 @@
+package Model;
+
+/**
+ *
+ * @author Nathan Gabriel
+ */
+abstract class Moedas {
+    private double taxaCompra, taxaVenda, cotacao, investimento;
+    public abstract void novaCotacao();
+}
