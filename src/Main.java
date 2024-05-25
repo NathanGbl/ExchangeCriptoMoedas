064013a4af3c2df05ccb892c2e28f88b3c64c854
unifnleite <unifnleite@P2LB0219.FEINET.FEI.EDU.BR>
2024-05-25 03:12:24 -0300

    Versão final apresentada.

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,6 @@
+
+import View.Login;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
@@ -9,6 +12,7 @@
  */
 public class Main {
     public static void main(String[] args) {
-        
+        Login login = new Login();
+        login.setVisible(true);
     }
 }
